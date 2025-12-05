ea56503bfbdf18c24fa84950bbf26a4973421d75
asamarka-625 <mix0x@mail.ru>
2025-12-05 09:40:10 +0300

    fix ip

--- web_app/src/dependencies/depends_ip.py
+++ web_app/src/dependencies/depends_ip.py
@@ -3,5 +3,15 @@ from fastapi import Request
 
 
 # Dependency для получения IP клиента
-def get_client_ip(request: Request) -> str:
-    return request.client.host or "no_ip"
+def get_real_client_ip(request: Request) -> str:
+    # Проверяем все возможные заголовки
+    forwarded_for = request.headers.get("X-Forwarded-For")
+    if forwarded_for:
+        # Берем первый IP (оригинальный клиент)
+        return forwarded_for.split(",")[0].strip()
+
+    real_ip = request.headers.get("X-Real-IP")
+    if real_ip:
+        return real_ip
+
+    return "no_ip"
